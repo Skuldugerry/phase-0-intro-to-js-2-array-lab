fa9d8da180eee8e11fe327c29e72af0322a64047
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-16 19:06:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // Write your solution here!
-let cats = [
-    "Milo",
-    "Otis",
-    "Garfield"
-];
-function destructivelyAppendCat(name){
+let cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(){
     cats.push("Ralph")
 }
 function destructivelyPrependCat(name){
@@ -18,5 +14,17 @@ function destructivelyRemoveFirstCat() {
 };
 function appendCat(name){
     let newCats= [...cats,"Broom"];
+    return newCats;
+};
+function prependCat(){
+    let newCats=["Arnold",...cats]
+    return newCats
+};
+function removeLastCat(){
+    const newCats=[cats.slice(0,cats.length -1)]
+    return newCats
+};
+function removeFirstCat(){
+    const newCats=[cats.slice(1,3)];
     return newCats;
 };
