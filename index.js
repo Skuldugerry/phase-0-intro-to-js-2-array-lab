d5aadcf98873cc5a8bfd4593bfa414c2bed5dfa5
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-16 18:40:57 +0300

    First changes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+let cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+};
+function destructivelyRemoveLastCat(){
+    cats.pop()
+};
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+};
+function appendCat(name){
+    let newCats= [...cats,"Broom"];
+    return newCats;
+};
